1bd60ff49b2c5300f908faa39defd5b9acdd5855
LL\sidbharg <siddharth.bhargava@publicissapient.com>
2021-05-23 19:01:28 +0530

    Adding address class and Address field to Employee class.

---
+++ src/streams/Address.java
@@ -0,0 +1,32 @@
+package streams;
+
+public class Address {
+
+    private String country;
+    private String city;
+    private int zipcode;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+}
